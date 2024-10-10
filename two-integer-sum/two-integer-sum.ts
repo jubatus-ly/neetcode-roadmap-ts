0a51c9041532baa8e4d6e0f44cd981f8107840e8
jubatus-ly <jubatus-ly@gmail.com>
2024-10-10 17:11:23 +0200

    two integer sum algorithm solved

--- two-integer-sum/two-integer-sum.ts
+++ two-integer-sum/two-integer-sum.ts
@@ -1,18 +1,25 @@
+// this solution was first done by a map but since the map do cannot duplicate keys 
+// and since we need to find the index so i got in trouble with the map
+// so i created a new object {value: number, diff: number} and my algorith worked
+// Time complexity: O(n^2)
+// Space complexity: O(n)
 class TwoIntegerSum implements SolutionTwoSum {
     twoSum(nums: number[], target: number): number[] {
-        let mapNums: Map<number, number> = new Map;
-        
+        let arrayNums: Array<{ value: number, diff: number }> = [];
         for(let i=0; i<nums.length; i++) {
-            mapNums.set(nums[i], Math.abs(nums[i]-target));
+            arrayNums.push({value: nums[i], diff: Math.abs(nums[i]-target)})
         }
-
-        for (let [key, val] of mapNums) {
-            if (mapNums.has(val)) {
-                const arrayMapNums = Array.from(mapNums.entries());
-                const index1 = arrayMapNums.findIndex(([k, v]) => k === key);
-                const index2 = arrayMapNums.findIndex(([k, v]) => k === val);
-                // findIndex index2 index1+1 step next
-                return [index1, index2]
+        console.log(arrayNums);
+        for (let {value, diff} of arrayNums) {
+            if (arrayNums.find((e) => e.diff === value)) {
+                const index1 = arrayNums.findIndex(e => e.value === value);
+                const index2 = arrayNums.findIndex((e, index) => {  
+                    if (index1 !== index && e.value === diff) {
+                        return true;
+                    }
+                    return false;
+                });
+                return [index1, index2];
             }
         }
         return [];
@@ -21,6 +28,5 @@ class TwoIntegerSum implements SolutionTwoSum {
 }
 
 // [3,4,5,6], 7
-
 const twoIntegerSum: TwoIntegerSum = new TwoIntegerSum();
-console.log(twoIntegerSum.twoSum([5,5,5], 10));
+console.log(twoIntegerSum.twoSum([4,5,5], 10));
